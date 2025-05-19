9f5bdfe1c4678cf62672c8f38c8613c210426c09
isaropesi <sil22028@byui.edu>
2025-05-18 21:19:17 -0300

    Refactor PersonQueue and PriorityQueue implementations; update tests for PriorityQueue functionality

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,23 +6,63 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_1()
+    // Scenario: Enqueue three items with different priorities and dequeue all.
+    // Expected Result: Items are dequeued in order of highest to lowest priority.
+    // Defect(s) Found: Dequeue does not remove the item from the queue, so repeated calls return the same item.
+    public void TestPriorityQueue_DifferentPriorities()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var pq = new PriorityQueue();
+        pq.Enqueue("A", 1);
+        pq.Enqueue("B", 3);
+        pq.Enqueue("C", 2);
+
+        Assert.AreEqual("B", pq.Dequeue());
+        Assert.AreEqual("C", pq.Dequeue());
+        Assert.AreEqual("A", pq.Dequeue());
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_2()
+    // Scenario: Enqueue items with the same priority and dequeue all.
+    // Expected Result: Items are dequeued in FIFO order.
+    // Defect(s) Found: If >= is used in Dequeue, the last inserted item is dequeued first (LIFO), not FIFO.
+    public void TestPriorityQueue_SamePriority_FIFO()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var pq = new PriorityQueue();
+        pq.Enqueue("A", 2);
+        pq.Enqueue("B", 2);
+        pq.Enqueue("C", 2);
+
+        Assert.AreEqual("A", pq.Dequeue());
+        Assert.AreEqual("B", pq.Dequeue());
+        Assert.AreEqual("C", pq.Dequeue());
+    }
+
+    [TestMethod]
+    // Scenario: Enqueue items with mixed priorities and dequeue some, then enqueue more and dequeue all.
+    // Expected Result: Always removes the highest priority, FIFO for ties, and works after more enqueues.
+    // Defect(s) Found: See above.
+    public void TestPriorityQueue_MixedOperations()
+    {
+        var pq = new PriorityQueue();
+        pq.Enqueue("A", 1);
+        pq.Enqueue("B", 3);
+        pq.Enqueue("C", 2);
+
+        Assert.AreEqual("B", pq.Dequeue());
+        pq.Enqueue("D", 3);
+        Assert.AreEqual("D", pq.Dequeue());
+        Assert.AreEqual("C", pq.Dequeue());
+        Assert.AreEqual("A", pq.Dequeue());
+    }
+
+    [TestMethod]
+    // Scenario: Dequeue from empty queue.
+    // Expected Result: Throws InvalidOperationException.
+    // Defect(s) Found: None.
+    public void TestPriorityQueue_Empty()
+    {
+        var pq = new PriorityQueue();
+        Assert.ThrowsException<InvalidOperationException>(() => pq.Dequeue());
     }
 
     // Add more test cases as needed below.
--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -1,12 +1,3 @@
-/// <summary>
-/// This queue is circular.  When people are added via AddPerson, then they are added to the 
-/// back of the queue (per FIFO rules).  When GetNextPerson is called, the next person
-/// in the queue is saved to be returned and then they are placed back into the back of the queue.  Thus,
-/// each person stays in the queue and is given turns.  When a person is added to the queue, 
-/// a turns parameter is provided to identify how many turns they will be given.  If the turns is 0 or
-/// less than they will stay in the queue forever.  If a person is out of turns then they will 
-/// not be added back into the queue.
-/// </summary>
 public class TakingTurnsQueue
 {
     private readonly PersonQueue _people = new();
@@ -37,17 +28,25 @@ public class TakingTurnsQueue
         {
             throw new InvalidOperationException("No one in the queue.");
         }
+
+        Person person = _people.Dequeue();
+
+        if (person.Turns <= 0)
+        {
+            // Infinite turns, just re-enqueue
+            _people.Enqueue(person);
+        }
         else
         {
-            Person person = _people.Dequeue();
-            if (person.Turns > 1)
+            // Decrement turns first
+            person.Turns--;
+            if (person.Turns > 0)
             {
-                person.Turns -= 1;
                 _people.Enqueue(person);
             }
-
-            return person;
         }
+
+        return person;
     }
 
     public override string ToString()
