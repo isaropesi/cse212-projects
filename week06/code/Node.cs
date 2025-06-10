d9254d5f66807814a2c45733cad289389853b45c
isaropesi <sil22028@byui.edu>
2025-06-10 20:03:50 -0300

    Implement reverse in-order traversal in BinarySearchTree; enhance Insert and Contains methods in Node class; complete InsertMiddle function in Trees class

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -11,11 +11,11 @@ public class Node
 
     public void Insert(int value)
     {
-        // TODO Start Problem 1
-
+        // Only insert unique values
+        if (value == Data)
+            return;
         if (value < Data)
         {
-            // Insert to the left
             if (Left is null)
                 Left = new Node(value);
             else
@@ -23,7 +23,6 @@ public class Node
         }
         else
         {
-            // Insert to the right
             if (Right is null)
                 Right = new Node(value);
             else
@@ -33,13 +32,18 @@ public class Node
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+        if (value < Data)
+            return Left != null && Left.Contains(value);
+        else
+            return Right != null && Right.Contains(value);
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
